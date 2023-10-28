449592dffb6351272916f2f8451903c02dea86c7
Shreyash Satish Killedar <144792072+shreyashkilledar@users.noreply.github.com>
2023-10-28 13:27:25 +0530

    Add files via upload

---
+++ Assignments/24-10-2023/diamond.java
@@ -0,0 +1,32 @@
+
+class diamond
+{
+	public static void main(String[] args)
+	{
+		int number = 10;
+		for (int i = 1; i <= number; i++) 
+        {
+			for (int j = 1; j <= number - i; j++) 
+            {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i * 2 - 1; k++) 
+            {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i = number - 1; i > 0; i--) 
+        {
+			for (int j = 1; j <= number - i; j++) 
+            {
+				System.out.print( " ");
+			}
+			for (int k = 1; k <= i * 2 - 1; k++) 
+            {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
